7f08a588334f83ab5396f5c336b9c786851b2746
han <2108136339@qq.com>
2023-03-18 22:52:56 +0800

    commit4_test

--- src/main/java/lenovo1.java
+++ src/main/java/lenovo1.java
@@ -1,5 +1,6 @@
 public class lenovo1 {
     public static void main(String[] args) {
         System.out.println("first test you test.");
+        System.out.println("second..............");
     }
 }
